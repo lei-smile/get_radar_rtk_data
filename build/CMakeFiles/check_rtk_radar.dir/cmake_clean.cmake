36233381e28824de6d1eb7dac9944029a2e04d43
lei.wu <15850650098@163.com>
2021-05-19 19:08:39 +0800

    在局部区域，提取有效的rtk数据与其对应毫米波数据，用于后续标定

---
+++ build/CMakeFiles/check_rtk_radar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check_rtk_radar.dir/src/check_rtk_radar.cpp.o"
+  "check_rtk_radar.pdb"
+  "check_rtk_radar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/check_rtk_radar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
